4cf9bc431a2d54e475ac3ad4cde55912702b4edb
Terracon82 <hyrumleishman@gmail.com>
2024-01-17 10:48:48 -0700

    Completed first version of Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = false;
+        do
+        {
+            Random randomNumberGenerator = new Random();
+            int magicNumber = randomNumberGenerator.Next(1, 11);
+
+            bool answerCorrect = false;
+            int attemptNumber = 0;
+            while (!answerCorrect)
+            {
+                Console.Write("What is your guess? ");
+                int numberGuess = int.Parse(Console.ReadLine());
+                attemptNumber++;
+
+                if (numberGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (numberGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (numberGuess == magicNumber)
+                {
+                    // Console.WriteLine("You guessed it!");
+                    answerCorrect = true;
+                    if (attemptNumber > 1)
+                    {
+                        Console.WriteLine($"You guessed it in {attemptNumber} attempts!");
+                    }
+                    else if (attemptNumber == 1)
+                    {
+                        Console.WriteLine($"You guessed it in {attemptNumber} attempt!");
+                    }
+                }
+
+
+            }
+
+            Console.Write("Play again? (y/n) ");
+            string playAgainResponse = Console.ReadLine();
+            if (playAgainResponse == "y")
+            {
+                playAgain = true;
+            }
+            else
+            {
+                playAgain = false;
+            }
+
+        } while (playAgain);
+
+
     }
 }
